9d4662b8679891bf5a2145defd01146c17873a0f
mlhench <54641506+mlhench@users.noreply.github.com>
2019-08-29 14:26:10 -0500

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,9 +1,4 @@
 class role::master_server {
   include profile::base
   include profile::agent_nodes
-  file { '/etc/docker/agent/Dockerfile':
-    content => "ENV HTTP_PROXY \"http://10.139.70.40:8080\"\n
-                ENV HTTPS_PROXY \"http://10.139.70.40:8080\"
-               "
-    }
 }
